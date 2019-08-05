cbc617352e8d8965df477e6ef747d5b852d3bb9a
dilrubajahan <djahanqa@gmail.com>
2019-08-05 00:55:55 -0400

    Contact page

--- src/main/java/controller/ContactPage.java
+++ src/main/java/controller/ContactPage.java
@@ -6,40 +6,55 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+import java.util.concurrent.TimeUnit;
 
 public class ContactPage extends ScriptBase {
-
     @FindBy(css = "#contact-link > a")
-    WebElement contactUs1;
+    WebElement contactUs;
     @FindBy(css = "#center_column > h1")
     WebElement costomerService;
-    @FindBy(css = "#id_contact")
-    WebElement subjectheading;
-    @FindBy(id="email")
+    @FindBy(css = "#contact-link > a")
+    WebElement contactUsclick;
+    @FindBy(id="id_contact")
+    WebElement subjectheadselect;
+
+    @FindBy(id ="email")
     WebElement emailinput;
-    @FindBy(id="id_order")
-    WebElement orderReference;
+    @FindBy (id="id_contact")
+    WebElement oderReference;
     @FindBy(id="message")
     WebElement message;
-
+    @FindBy (css = "#submitMessage > span")
+    WebElement clickSend;
+    @FindBy(css = "#center_column > div > p")
+    WebElement oneErrorfound;
 
     public ContactPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
 
-public void messagesent(String email,String reference,String messages){
-        contactUs1.click();
-        costomerService.isDisplayed();
-        dropdown(subjectheading,"Webmaster");
-        emailinput.sendKeys(email);
-        orderReference.sendKeys(reference);
-        message.sendKeys(messages);
+    public void customerService() {
+        contactUs.click();
+        Assert.assertEquals(costomerService, costomerService);
 
     }
-public void dropdown(WebElement element,String value){
-        element=subjectheading;
+    public void sendmessage(String email,String reference,String messages,String value) {
+        contactUsclick.click();
+        subjectheadselect.isSelected();
+        emailinput.sendKeys(email);
+        oderReference.sendKeys(reference);
+        message.sendKeys(messages);
+        clickSend.click();
+        Assert.assertEquals(oneErrorfound,oneErrorfound);
+    }
+
+
+    public void dropdown(WebElement element,String value){
+        element= subjectheadselect;
         Select select=new Select(element);
         select.selectByVisibleText(value);
-}
 
+    }
 }
