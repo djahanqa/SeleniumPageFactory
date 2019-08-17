5f84cb265e4ac0a5e14d148ee1a5668970fd45d2
dilrubajahan <djahanqa@gmail.com>
2019-08-17 18:57:59 -0400

    woman page added

--- src/test/java/woman/WomanTest.java
+++ src/test/java/woman/WomanTest.java
@@ -1,4 +1,45 @@
 package woman;
 
-public class WomanTest {
+import base.ScriptBase;
+import controller.Woman;
+import controller.WomanCatalog;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class WomanTest extends ScriptBase {
+
+    Woman woman;
+
+    @BeforeTest
+    public void beforetest() {
+        init();
+    }
+
+    @Test
+    public void verifyTshirt() {
+        woman = new Woman(driver);
+        woman.topssearch(driver,"T-shirts");
+
+    }
+
+    @Test
+    public void verifyblouse() {
+        woman= new Woman(driver);
+        woman.topssearch(driver,"Blouses");
+    }
+
+
+
+    @AfterTest
+    void closebrowser() {
+
+
+        driver.close();
+        driver.quit();
+    }
+
+
 }
+
+
