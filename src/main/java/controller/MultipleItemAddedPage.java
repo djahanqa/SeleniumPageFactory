911827e96fd6022197b34303a1b0e2f0d5aa9d39
dilrubajahan <djahanqa@gmail.com>
2019-08-28 15:00:15 -0400

    mutiple items added to shopping cart

--- src/main/java/controller/MultipleItemAddedPage.java
+++ src/main/java/controller/MultipleItemAddedPage.java
@@ -11,6 +11,21 @@ public class MultipleItemAddedPage extends ScriptBase {
 
     @FindBy(xpath = "//*[@id='homefeatured']/li[1]//h5[@itemprop='name']")
     WebElement fadedTshirt;
+    @FindBy(xpath = "//*/button[@name='Submit']")
+    WebElement addtocart;
+
+    @FindBy (xpath = "//*[@id='layer_cart']//span[@title='Continue shopping']")
+    WebElement continueshopping;
+    @FindBy (xpath = "//div/a[@title='View my shopping cart']")
+    WebElement cartPageView;
+    @FindBy (xpath = "//*[@id='header_logo']/a/img[@class='logo img-responsive']")
+    WebElement homepage;
+    @FindBy(xpath = "//*[@id='homefeatured']//a/img[@title='Blouse']")
+    WebElement blouse;
+    @FindBy(xpath = "//*[@id='homefeatured']/li[2]//a[@title='Add to cart']")
+    WebElement addToCart;
+    @FindBy (css = "#button_order_cart > span")
+    WebElement checkout;
 
 
 
@@ -19,13 +34,25 @@ public MultipleItemAddedPage (WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
 
-    public void itemAdded (WebDriver driver) {
+    public void itemAdded (WebDriver driver) throws InterruptedException {
     mouseOver(fadedTshirt,driver);
     fadedTshirt.click();
+    addtocart.click();
+    Thread.sleep(5000);
+    continueshopping.click();
+    mouseOver(cartPageView,driver);
+    Thread.sleep(5000);
+    checkout.click();
+    homepage.click();
+    mouseOver(blouse,driver);
+    addToCart.click();
+    Thread.sleep(5000);
 
 
-
-
+    //mouseOver(blouse,driver);
+    //blouse.click();
+    //addToCart.click();
+    //Thread.sleep(5000);
 
     }
 
